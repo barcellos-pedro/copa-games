ce5993f6327b73e7784f36d5eb75e475d9cd187f
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-07-10 18:37:08 -0300

    Create selecao component

---
+++ copa-lambda/src/app/selecao/selecao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selecao',
+  templateUrl: './selecao.component.html',
+  styleUrls: ['./selecao.component.scss']
+})
+export class SelecaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
