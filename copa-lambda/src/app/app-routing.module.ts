99021ed15cd762ef7571268e2d2d7f820f829f42
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-07-10 18:36:52 -0300

    Update modules

--- copa-lambda/src/app/app-routing.module.ts
+++ copa-lambda/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SelecaoComponent } from './selecao/selecao.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "pagina-inicial", pathMatch: "full" },
   { path: "pagina-inicial", component: HomeComponent },
+  { path: "selecao", component: SelecaoComponent }
 ];
 
 @NgModule({
