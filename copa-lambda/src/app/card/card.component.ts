0c6d953f35b9f1bcd15bd6036a9cb7124c0f370b
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-07-10 21:37:47 -0300

    Implement card props

--- copa-lambda/src/app/card/card.component.ts
+++ copa-lambda/src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() game: Game;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  retornaConsole({ titulo }): string {
+    return titulo.slice(titulo.indexOf('(') + 1, titulo.lastIndexOf(')'));
   }
 
 }
