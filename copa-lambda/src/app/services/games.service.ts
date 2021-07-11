e0fe153e20a7f10748a301039ab90b1c2b23414b
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-07-10 21:01:03 -0300

    Create method

--- copa-lambda/src/app/services/games.service.ts
+++ copa-lambda/src/app/services/games.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Game } from 'src/models/game';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesService {
+  readonly endpoint: string = "https://l3-processoseletivo.azurewebsites.net/api/Competidores?copa=games";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Return all games
+   * @returns Observable<Games[]>
+   */
+  getGames(): Observable<Game[]> {
+    return this.http.get(this.endpoint).pipe<Game[]>(
+      catchError<any, any>(error => of(undefined))
+    );
+  }
+
 }
