7a7646b32ded9f55f8bb85309f8fe5f79634c1cc
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-07-10 21:38:12 -0300

    Fetch games from API

--- copa-lambda/src/app/selecao/selecao.component.ts
+++ copa-lambda/src/app/selecao/selecao.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, of, Subject } from "rxjs";
+import { catchError, takeUntil } from "rxjs/operators";
+import { Game } from 'src/models/game';
+import { GamesService } from '../services/games.service';
 @Component({
   selector: 'app-selecao',
   templateUrl: './selecao.component.html',
   styleUrls: ['./selecao.component.scss']
 })
-export class SelecaoComponent implements OnInit {
+export class SelecaoComponent implements OnInit, OnDestroy {
+  games$: Observable<Array<Game>>;
+  loadingError$ = new Subject<boolean>();
+  componentDestroyed$: Subject<boolean> = new Subject();
 
-  constructor() { }
+  constructor(private service: GamesService) { }
 
   ngOnInit(): void {
+    this.games$ = this.service.getGames().pipe(
+      takeUntil(this.componentDestroyed$),
+      catchError<any, any>((error) => {
+        console.log('deu ruim')
+        this.loadingError$.next(true);
+        return of();
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next(true);
+    this.componentDestroyed$.complete();
   }
 
 }
