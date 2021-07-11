04f80b69dbcb326dc75f3703566d8b1a82a8ba4e
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-07-11 14:35:28 -0300

    Use service methods

--- copa-lambda/src/app/card/card.component.ts
+++ copa-lambda/src/app/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Game } from 'src/models/game';
+import { GamesService } from '../services/games.service';
 
 @Component({
   selector: 'app-card',
@@ -12,17 +13,9 @@ export class CardComponent implements OnInit {
   @Output() notifyValue: EventEmitter<any> = new EventEmitter();
   checked: boolean;
 
-  constructor() { }
+  constructor(public service: GamesService) { }
 
   ngOnInit(): void {
-  }
-
-  retornaTitulo({ titulo }): string {
-    return titulo.slice(0, titulo.indexOf('(')).trim();
-  }
-
-  retornaConsole({ titulo }): string {
-    return titulo.slice(titulo.indexOf('(') + 1, titulo.lastIndexOf(')'));
   }
 
   onChange(game: Game, checked) {
