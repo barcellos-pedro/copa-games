a4a1623b8b1e67efca836d53bbfd1226ac6483dc
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-07-10 23:12:07 -0300

    Update module

--- copa-lambda/src/app/app.module.ts
+++ copa-lambda/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { GamesService } from './services/games.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [GamesService],
   bootstrap: [AppComponent]
