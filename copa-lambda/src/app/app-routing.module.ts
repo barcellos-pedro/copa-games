937081bfc634d83533aae805d3888d7618ef1186
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-07-11 14:30:37 -0300

    Add result route

--- copa-lambda/src/app/app-routing.module.ts
+++ copa-lambda/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ResultadoComponent } from './resultado/resultado.component';
 import { SelecaoComponent } from './selecao/selecao.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "pagina-inicial", pathMatch: "full" },
   { path: "pagina-inicial", component: HomeComponent },
-  { path: "selecao", component: SelecaoComponent }
+  { path: "selecao", component: SelecaoComponent },
+  { path: "resultado", component: ResultadoComponent }
 ];
 
 @NgModule({
