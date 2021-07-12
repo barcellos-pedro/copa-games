0e1cb7ca815eb5c405c9572f677f1c081d1f0da7
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-07-11 21:09:09 -0300

    Implement final result component

--- copa-lambda/src/app/resultado-final/resultado-final.component.ts
+++ copa-lambda/src/app/resultado-final/resultado-final.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin, from, Observable, of, Subject } from 'rxjs';
+import { catchError, map, takeUntil } from 'rxjs/operators';
+import { Game } from 'src/models/game';
+import { GamesService } from '../services/games.service';
 
 @Component({
   selector: 'app-resultado-final',
   templateUrl: './resultado-final.component.html',
   styleUrls: ['./resultado-final.component.scss']
 })
-export class ResultadoFinalComponent implements OnInit {
+export class ResultadoFinalComponent implements OnInit, OnDestroy {
+  games$: Observable<Game[]>;
+  winnerId: string;
+  secondPlaceId: string;
+  componentDestroyed$: Subject<boolean> = new Subject();
+  loadingError$ = new Subject<boolean>();
 
-  constructor() { }
+  constructor(public service: GamesService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getFinalists();
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next(true);
+    this.componentDestroyed$.complete();
+  }
+
+  getFinalists(): void {
+    this.winnerId = this.route.snapshot.paramMap.get('winner');
+    this.secondPlaceId = this.route.snapshot.paramMap.get('secondPlace');
+    
+    console.log([this.winnerId, this.secondPlaceId]);
+
+    this.games$ = forkJoin({
+      winner: this.service.getGameById(this.winnerId),
+      secondPlace: this.service.getGameById(this.secondPlaceId)
+    }).pipe(
+      takeUntil(this.componentDestroyed$),
+      map(resp => [resp.winner, resp.secondPlace])
+    );
   }
 
 }
